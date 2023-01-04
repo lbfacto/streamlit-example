bb8254d389f4116ed794757c32b834536568fc5b
lbfacto <48971071+lbfacto@users.noreply.github.com>
2023-01-04 19:07:30 +0000

    Add files via upload

--- palu.py
+++ palu.py
@@ -15,7 +15,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('model/trained_model.sav','rb'))
+loaded_model = pickle.load(open('trained_model.pkl','rb'))
 
 
 # creating a function for Prediction
